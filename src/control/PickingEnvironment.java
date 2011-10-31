996b97cf700726720e576c1d7a4db4cb9ee2001b
Nicolas <nicolas.bouillon@eleves.ec-nantes.fr>
2011-10-31 17:36:26 +0100

    Change mouseAdapter to mouseListener which is an interface: it allows to use mousemotionListener and its mouseDragged method

--- src/control/PickingEnvironment.java
+++ src/control/PickingEnvironment.java
@@ -1,19 +1,21 @@
 package control;
 
 import java.awt.Frame;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
-import javax.vecmath.Point3d;
 
 import view.TriangleViewer;
+
 import com.sun.j3d.utils.picking.PickCanvas;
 import com.sun.j3d.utils.picking.PickResult;
 
-public class PickingEnvironment extends MouseAdapter {
+public class PickingEnvironment implements MouseListener,MouseMotionListener  {
 
 	PickCanvas pickCanvas;
 	NewMouseRotate mouseRotate = null;
@@ -30,6 +32,8 @@ public class PickingEnvironment extends MouseAdapter {
 		pickCanvas = new PickCanvas(c, group);
 		pickCanvas.setMode(PickCanvas.BOUNDS);
 		c.addMouseListener(this);
+		c.addMouseMotionListener(this);
+
 
 	}
 
@@ -43,21 +47,60 @@ public class PickingEnvironment extends MouseAdapter {
 
 	public void mouseClicked(MouseEvent e) {
 		
-		pickCanvas.setShapeLocation(e);
-		PickResult result = pickCanvas.pickClosest();
-		if (result == null) {
-			System.out.println("Nothing picked");
-		} else {
-			TriangleViewer s = (TriangleViewer) result.getNode(PickResult.SHAPE3D);
-			if (s != null) {
-				System.out.println(s.getClass().getName());
-				s.selectOrUnselect();
-				s.changeColor();
-				mouseRotate.setCenter(s);
+		int buttonDown = e.getButton();
+		 
+	    if (buttonDown == MouseEvent.BUTTON1) {
+	           // Bouton GAUCHE enfoncé
+			pickCanvas.setShapeLocation(e);
+			PickResult result = pickCanvas.pickClosest();
+			if (result == null) {
+				System.out.println("Nothing picked");
 			} else {
-				System.out.println("null");
+				TriangleViewer s = (TriangleViewer) result.getNode(PickResult.SHAPE3D);
+				if (s != null) {
+					System.out.println(s.getClass().getName());
+					s.selectOrUnselect();
+					s.changeColor();
+					mouseRotate.setCenter(s);
+				} else {
+					System.out.println("null");
+				}
 			}
-		}
+	    }
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		System.out.println("test3");
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
 	}
 
 }
